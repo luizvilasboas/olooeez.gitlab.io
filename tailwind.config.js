ae2895dd8c49d448358c08a1253588a5b307fd8a
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-01-10 19:10:22 -0300

    Configure basic tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundBlack: '#27272a',
+        backagroundWhite: '#e4e4e7',
+        textBlack: '#27272a',
+        textWhite: '#e4e4e7',
+        hoverBlack: '#cbd5e1',
+        hoverWhite: '#52525b',
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
