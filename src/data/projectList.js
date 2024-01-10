fcd8be1de8697cacb2438513c321978d1a48c5cc
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-01-10 19:10:07 -0300

    Separate data from component

---
+++ src/data/projectList.js
@@ -0,0 +1,29 @@
+export const projectList = [
+  {
+    id: 1,
+    title: 'Budget Alchemy',
+    demoLink: 'https://budget-alchemy.netlify.app',
+    codeLink: 'https://gitlab.com/olooeez/budget-alchemy',
+    description:
+      'Plataforma online dedicada ao gerenciamento abrangente de finanças pessoais, oferecendo recursos para monitoramento de despesas e orçamento.',
+    tags: ['React', 'Material UI'],
+  },
+  {
+    id: 2,
+    title: 'Stay Fit',
+    demoLink: 'https://stay-fit-reactjs.netlify.app/',
+    codeLink: 'https://gitlab.com/olooeez/stay-fit',
+    description:
+      'Plataforma online destinada ao treinamento, visualização e exploração de exercícios físicos, permitindo a busca por nome ou parte específica do corpo a ser trabalhada.',
+    tags: ['React', 'Material UI', 'RapidAPI'],
+  },
+  {
+    id: 3,
+    title: 'Snapcorrect',
+    codeLink: 'https://gitlab.com/ufv-projects/hackathon-2023',
+    demoLink: null,
+    description:
+      'Site de correção de provas por marcação, 2º lugar na hackathon 2023 de visão computacional pela Pix Force. Gera relatório de acertos e erros em uma plataforma que faz o gerenciamento de notas.',
+    tags: ['Flask', 'Tailwindcss', 'OpenCV', 'Numpy'],
+  },
+]
